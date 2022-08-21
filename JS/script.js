9f539cbd61faee0ac6d92dc801703b5df6ff1cb8
davyrap <davide.rap04@gmail.com>
2022-08-21 11:48:54 +0200

    Aggiunta pagina preferenze
    Migliorie alla credibilità, aggiunta pagina preferenze e funzionalità al tasto X

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
 function loadForm() {
     window.open("https://docs.google.com/forms/d/1rHsdTEvHGxVnCmgNpwpSV2_672NPDR9iMVmw-0QUDhc/edit", "_self");
 }
+
+function loadPref() {
+    window.open("pages/preferenze.html", "_self")
+}
